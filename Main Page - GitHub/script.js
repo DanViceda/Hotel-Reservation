cd9f64898d1f33545102fd8989b0fe1089352290
Danniella Viceda <vicedadanniella@gmail.com>
2023-11-26 23:08:34 -0800

    modified:   Landing Page - GitHub/index.html new file:   Main Page - GitHub/img/1.jpg new file:   Main Page - GitHub/img/background.jpg new file:   Main Page - GitHub/img/background1.jpg new file:   Main Page - GitHub/img/image1.jpg new file:   Main Page - GitHub/img/image2.jpg new file:   Main Page - GitHub/img/image3.jpg new file:   Main Page - GitHub/img/image4.jpg new file:   Main Page - GitHub/img/image5.jpg new file:   Main Page - GitHub/img/image6.jpg new file:   Main Page - GitHub/img/logo.png new file:   Main Page - GitHub/img/pic-1.jpg new file:   Main Page - GitHub/img/pic-2.jpeg new file:   Main Page - GitHub/img/pic-3.jpg new file:   Main Page - GitHub/img/room.jpg new file:   Main Page - GitHub/img/room1.jpg new file:   Main Page - GitHub/img/room2.jpg new file:   Main Page - GitHub/img/room3.jpg new file:   Main Page - GitHub/img/room4.jpg new file:   Main Page - GitHub/img/service.jpg new file:   Main Page - GitHub/index.html new file:   Main Page - GitHub/script.js new file:   Main Page - GitHub/style.css

---
+++ Main Page - GitHub/script.js
@@ -0,0 +1,68 @@
+
+/*Navigator*/
+let btnMenu = document.getElementById('btn-menu');
+let menu1 = document.getElementById('menu');
+
+btnMenu.onclick = () =>{
+    btnMenu.classList.toggle('fa-times');
+    menu1.classList.toggle('active')
+}  
+
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  let reveals = document.querySelectorAll('.reveal');
+
+  for(let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 150;
+
+    if(revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+// JavaScript to create a testimonial carousel with navigation buttons
+const carousel = document.querySelector('.carousel');
+const testimonials = document.querySelectorAll('.testimonial');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+let currentIndex = 0;
+
+function showTestimonial(index) {
+    testimonials.forEach((testimonial, i) => {
+        if (i === index) {
+            testimonial.style.display = 'block';
+        } else {
+            testimonial.style.display = 'none';
+        }
+    });
+}
+
+function prevTestimonial() {
+    currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+    showTestimonial(currentIndex);
+}
+
+function nextTestimonial() {
+    currentIndex = (currentIndex + 1) % testimonials.length;
+    showTestimonial(currentIndex);
+}
+
+showTestimonial(currentIndex);
+
+prevButton.addEventListener('click', prevTestimonial);
+nextButton.addEventListener('click', nextTestimonial);
+
+// Navigation bar effect
+
+window.addEventListener("scroll", function() {
+  const navbar = document.getElementsByClassName("header-nav")[0];
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+});
